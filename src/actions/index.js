d5a6c009c98d84f9b79dd8511e3464d538564ecd
Marco K <marcokuang@users.noreply.github.com>
2018-10-05 14:59:39 -0700

    select book is working

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,9 @@
 export function selectBook(book){
-    console.log('A book is selected: ' + book.title);
+    // console.log('A book is selected: ' + book.title);
+    //selectBook is an action creator, it needs to return an action, object with a type property
+    // payload has more information about the returned object.
+    return {
+        type: 'BOOK_SELECTED',
+        payload: book
+    }
 }
