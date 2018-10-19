860258ec5bb0fce415b11b23882e83c6b86727ed
Marco K <marcokuang@users.noreply.github.com>
2018-10-18 19:58:39 -0700

    added showing pages

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function (){
     return [
-        {title: 'Javascript'},
-        {title: 'Java'},
-        {title: 'Swift'},
-        {title: 'Android'},
-        {title: 'Rust'}
+        {title: 'Javascript', pages: 123},
+        {title: 'Java', pages: 324},
+        {title: 'Swift', pages: 150},
+        {title: 'Android', pages: 887},
+        {title: 'Rust', pages: 239}
     ]
 }
